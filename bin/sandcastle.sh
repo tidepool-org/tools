a090e5ba0e1bade14a4a2f4313ec0059be8b8ae8
Ben West <bewest@gmail.com>
2014-02-15 00:38:26 -0800

    add foreman

--- bin/sandcastle.sh
+++ bin/sandcastle.sh
@@ -5,4 +5,5 @@ ETC="./etc"
 ENVSTR=$(load_env $ETC/common.env $ETC/sandcastle.env)
 echo starting $NODE $0
 export BASE="$(pwd)/$BASE"
-env $(echo "$ENVSTR") $NODE node_modules/sandcastle/server.js 2>&1 | tee server.log
+echo "USING $BASE"
+(env $(echo "$ENVSTR") $NODE node_modules/sandcastle/server.js 2>&1 ) | tee server.log
