353fddbece404af6f3eaa12a657b81b2546d4d97
kentquirk <kentquirk@gmail.com>
2014-05-13 08:04:36 -0400

    Add some tools to make managing our list of repositories easier.

---
+++ get_current_tidepool_repos.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+# This script depends on a list of current repositories found in tools/current_repos.txt
+
+if [ -f runservers ]; then
+  cd ..
+fi
+
+if [ ! -d tools ]; then
+  repos=( $(cat "tools/required_repos.txt") )  #  Stores contents of that file in an array.
+  echo "You should be in the tools directory or its immediate parent directory to run this."
+fi
+
+
+get_one_tidepool_repo()
+{
+    echo "*** $1 ***"
+    if [ -d "$1" ]; then
+        echo "Skipping $1 because there is already a directory by that name."
+    else
+        git clone git@github.com:tidepool-org/$1.git
+        cd $1
+        if [ -f package.json ]; then
+            npm install
+        fi
+        if [ -f bower.json ]; then
+            bower install
+        fi
+    fi
+}
+
+for elt in $(seq 0 $((${#repos[@]} - 1))); do
+    get_one_tidepool_repo ${repos[elt]}
+done
---
+++ update_current_tidepool_repos.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+# This script depends on a list of current repositories found in tools/current_repos.txt
+
+if [ -f runservers ]; then
+  cd ..
+fi
+
+if [ ! -d tools ]; then
+  repos=( $(cat "tools/required_repos.txt") )  #  Stores contents of that file in an array.
+  echo "You should be in the tools directory or its immediate parent directory to run this."
+fi
+
+update_one_tidepool_repo()
+{
+    echo "*** $1 ***"
+    if [ ! -d "$1" ]; then
+        echo "Cloning $1 because it seems to be missing."
+        git clone git@github.com:tidepool-org/$1.git
+    fi
+
+    if [ -d "$1" ]; then
+        cd $1
+        git fetch --prune --tags
+        git pull
+        if [ -f package.json ]; then
+            npm install
+        fi
+        if [ -f bower.json ]; then
+            bower install
+        fi
+        cd ..
+    fi
+}
+
+for elt in $(seq 0 $((${#repos[@]} - 1))); do
+    update_one_tidepool_repo ${repos[elt]}
+done
