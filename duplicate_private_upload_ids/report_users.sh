e59302acdc095fc8a71ef6842ec3a606c9f59a82
Darin Krauss <darinkrauss@gmail.com>
2015-08-18 14:02:21 -0700

    Add documentation to report_users.sh

--- duplicate_private_upload_ids/report_users.sh
+++ duplicate_private_upload_ids/report_users.sh
@@ -3,6 +3,49 @@
 # Requires: mongo, base64, openssl, jq
 # base64 - works on both ubuntu and macos
 
+# Requirements:
+#
+# The following support tools are required:
+#
+# mongo - database; already required by Tidepool
+# base64 - BASE64 encoding/decoding; common on most *nix distributions
+# openssl - AES256 encryption/decryption; common on most *nix distributions
+# jq - JSON parsing; use `brew install jq` to install on Mac
+#
+# The following environment variable is required:
+#
+# SALT_DEPLOY - export of the Seagull SALT_DEPLOY for the target environment
+#
+# To export this environment variable:
+#
+# $ read -s SALT_DEPLOY
+# <copy-paste-the-seagull-salt-deploy-and-press-return>
+# $ export SALT_DEPLOY
+#
+# Parameters:
+#
+# 1 - target environment; one of 'prod', 'staging', 'devel', 'test', 'local';
+#     'test' environment is only for an AWS cluster without SSL-enabled mongo
+# 2 - optional user_id; if supplied report only on the specified user; otherwise
+#     report on all users
+#
+# Output:
+#
+# The output columns are:
+#
+# username | user_id | metadata_id | metadata_hash | private_uploads_id | private_uploads_hash | private_uploads_count | private_uploads_no_device_id_count | private_uploads_upload_ids | private_uploads_device_ids
+#
+# username - the username of the user
+# user_id - the id of the user
+# metadata_id - the id for the metadata associated with the user
+# metadata_hash - the encryption hash for the metadata associated with the user
+# private_uploads_id - the _groupId for all device data for the user
+# private_uploads_hash - (currently unused)
+# private_uploads_count - total count of all device data records for the user
+# private_uploads_no_device_id_count - total count of all device data records for the user that do not have a deviceId property
+# private_uploads_upload_ids - JSON array; all uploadIds for all device data for the user
+# private_uploads_device_ids - JSON array; all deviceIds for all device data for the user
+
 if [ -z "$(which mongo)" -o -z "$(which base64)" -o -z "$(which openssl)" -o -z "$(which jq)" ]; then
   echo "ERROR: Required tools: mongo, base64, openssl, jq" >&2
   exit 1
