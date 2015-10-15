00fa0367baf0df1899d96c38ae133c5862521a0a
Darin Krauss <darinkrauss@gmail.com>
2015-10-15 13:19:52 -0700

    Migrate encrypted metadata in Seagull from old to new SALT_DEPLOY

---
+++ migrate_metadata/README.md
@@ -0,0 +1,33 @@
+This script migrates user metadata from an old SALT_DEPLOY to a new SALT_DEPLOY
+for Seagull. The Seagull process should be stopped and user access to the
+environment prohibited.
+
+Note: Backup the Mongo database before executing
+
+In a terminal on the Mongo master node for the specified environment, execute the following commands:
+
+```
+# Set ENVIRONMENT (prod|staging|devel|test|local)
+export ENVIRONMENT=prod
+
+# Set seagull OLD_SALT_DEPLOY
+$ read -s OLD_SALT_DEPLOY
+<enter seagull old SALT_DEPLOY here>
+$ export OLD_SALT_DEPLOY
+
+# Set seagull NEW_SALT_DEPLOY
+$ read -s NEW_SALT_DEPLOY
+<enter seagull new SALT_DEPLOY here>
+$ export NEW_SALT_DEPLOY
+
+# Set MONGO_OPTIONS to host and port of mongo master connection
+# If not specified, defaults to localhost
+$ export MONGO_OPTIONS="--host localhost --port 27017"    # Use environment specific access points
+
+# Stop Seagull (and presumably all user access)
+
+# Migrate the metadata
+$ ./migrate_metadata.sh ${ENVIRONMENT} 2> migrate_metadata.err | tee migrate_metadata.out
+
+# Start Seagull with the new SALT_DEPLOY (and presumably all user access)
+```
---
+++ migrate_metadata/migrate_metadata.sh
@@ -0,0 +1,135 @@
+#!/bin/bash -eu
+
+# Requires: mongo, base64, openssl
+# base64 - works on both ubuntu and macos
+
+if [ -z "$(which mongo)" -o -z "$(which base64)" -o -z "$(which openssl)" ]; then
+  echo "ERROR: Required tools: mongo, base64, openssl" >&2
+  exit 1
+fi
+
+if [ -z "$(base64 --help | grep '\-\-wrap=')" ]; then
+  BASE64_OPTIONS=
+else
+  BASE64_OPTIONS="--wrap=0"
+fi
+
+# OLD_SALT_DEPLOY, NEW_SALT_DEPLOY environment variables must be exported outside of this script
+if [ -z "${OLD_SALT_DEPLOY:-}" -o -z "${NEW_SALT_DEPLOY:-}" ]; then
+  echo "ERROR: Required environment variables: OLD_SALT_DEPLOY, NEW_SALT_DEPLOY" >&2
+  exit 1
+fi
+
+# Environment
+if [ -z "${1:-}" ]; then
+  echo "ERROR: First argument must be environment: prd, prod, staging, devel, test, local" >&2
+  exit 1
+else
+  environment="${1}"
+fi
+case "${environment}" in
+  prd)
+    MONGO_OPTIONS="${MONGO_OPTIONS:-} --ssl --sslAllowInvalidCertificates --quiet"
+    USERS_DATABASE="user"
+    SEAGULL_DATABASE="seagull"
+    ;;
+  prod)
+    MONGO_OPTIONS="${MONGO_OPTIONS:-} -ssl --quiet"
+    USERS_DATABASE="user"
+    SEAGULL_DATABASE="seagull"
+    ;;
+  staging)
+    MONGO_OPTIONS="${MONGO_OPTIONS:-} -ssl --quiet"
+    USERS_DATABASE="user_staging"
+    SEAGULL_DATABASE="seagull_staging"
+    ;;
+  devel)
+    MONGO_OPTIONS="${MONGO_OPTIONS:-} -ssl --quiet"
+    USERS_DATABASE="user"
+    SEAGULL_DATABASE="seagull"
+    ;;
+  test)
+    MONGO_OPTIONS="${MONGO_OPTIONS:-} --quiet"
+    USERS_DATABASE="user"
+    SEAGULL_DATABASE="seagull"
+    ;;
+  local)
+    MONGO_OPTIONS="${MONGO_OPTIONS:-} --quiet"
+    USERS_DATABASE="user"
+    SEAGULL_DATABASE="user"
+    ;;
+  *)
+    echo "ERROR: First argument must be environment: prd, prod, staging, devel, test, local" >&2
+    exit 1
+esac
+
+shift
+
+migrate_user_metadata()
+{
+  unset username
+  unset user_id
+  unset metadata_id
+  unset metadata_hash
+  unset old_secret_key
+  unset new_secret_key
+  unset metadata_encrypted
+  unset metadata_decrypted
+
+  IFS=\| read username user_id metadata_id metadata_hash <<< "${1}"
+  if [ ${#username} -lt 1 -o ${#user_id} -ne 10 -o ${#metadata_id} -ne 10 -o ${#metadata_hash} -ne 24 ]; then
+    echo "WARN: Ignoring non-standard user information: ${user}" >&2
+    return
+  fi
+
+  old_secret_key="$(printf "${metadata_hash}${OLD_SALT_DEPLOY}" | openssl dgst -sha256 -hex | sed 's/.* //')"
+  if [ ${#old_secret_key} -lt 1 ]; then
+    echo "ERROR: Failure to calculate old secret key for user: ${user}" >&2
+    return
+  fi
+
+  new_secret_key="$(printf "${metadata_hash}${NEW_SALT_DEPLOY}" | openssl dgst -sha256 -hex | sed 's/.* //')"
+  if [ ${#new_secret_key} -lt 1 ]; then
+    echo "ERROR: Failure to calculate new secret key for user: ${user}" >&2
+    return
+  fi
+
+  metadata_encrypted="$(mongo ${MONGO_OPTIONS} ${SEAGULL_DATABASE} --eval "db.seagull.find({_id: \"${metadata_id}\"}).forEach(function(f) { print(f.value); })")"
+  if [ ${#metadata_encrypted} -lt 1 ]; then
+    echo "WARN: Ignoring missing encrypted metadata for user: ${user}" >&2
+    return
+  fi
+
+  metadata_decrypted="$(printf "${metadata_encrypted}" | base64 --decode | openssl enc -d -aes256 -k "${old_secret_key}")" # | sed 's/\\"/\\\\"/g')"
+  if [ ${#metadata_decrypted} -lt 1 ]; then
+    echo "WARN: Ignoring missing decrypted metadata for user: ${user}" >&2
+    return
+  fi
+
+  metadata_encrypted="$(printf "${metadata_decrypted}" | openssl enc -e -aes256 -k "${new_secret_key}" | base64 ${BASE64_OPTIONS})"
+  if [ ${#metadata_encrypted} -lt 1 ]; then
+    echo "ERROR: Invalid encrypted updated metadata for user: ${user}" >&2
+    return
+  fi
+
+  mongo ${MONGO_OPTIONS} ${SEAGULL_DATABASE} --eval "db.seagull.update({_id: \"${metadata_id}\"}, {_id: \"${metadata_id}\", value: \"${metadata_encrypted}\"})"
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Failure to update mongo for user: ${user}" >&2
+    return
+  fi
+
+  echo "${username} ${user_id}"
+}
+
+migrate_metadata()
+{
+  echo "PROCESSING USER METADATA" >&2
+
+  users="$(mongo ${MONGO_OPTIONS} ${USERS_DATABASE} --eval "db.users.find().forEach(function(f) { print(f.username + '|' + f.userid + '|' + (f.private && f.private.meta ? f.private.meta.id : '') + '|' + (f.private && f.private.meta ? f.private.meta.hash : '')); })")"
+  echo "${users}" | while read -r user; do
+    migrate_user_metadata "${user}"
+    sleep 0.05
+  done
+}
+
+migrate_metadata
