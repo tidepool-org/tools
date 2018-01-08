31e746d96bf06ac940d9b3422a5f4cee6ae98c0f
Darin Krauss <darinkrauss@gmail.com>
2018-01-08 14:36:16 -0800

    Add Docker build to artifact scripts

--- artifact/artifact_go.sh
+++ artifact/artifact_go.sh
@@ -1,26 +1,39 @@
-#!/bin/sh -e
-
-if [ -z "${TRAVIS_TAG}" ]; then
-    exit 0
-fi
-
-set -u
+#!/bin/bash -eu
 
 if [ "${TRAVIS_GO_VERSION}" != "${ARTIFACT_GO_VERSION}" ]; then
     exit 0
 fi
 
-ARTIFACT_DIR='deploy'
+if [ -n "${TRAVIS_TAG:-}" ]; then
+    ARTIFACT_DIR='deploy'
 
-APP="${TRAVIS_REPO_SLUG#*/}"
-APP_DIR="${ARTIFACT_DIR}/${APP}"
-APP_TAG="${APP}-${TRAVIS_TAG}"
+    APP="${TRAVIS_REPO_SLUG#*/}"
+    APP_DIR="${ARTIFACT_DIR}/${APP}"
+    APP_TAG="${APP}-${TRAVIS_TAG}"
 
-rm -rf "${ARTIFACT_DIR}/" || { echo 'ERROR: Unable to delete artifact directory'; exit 1; }
-mkdir -p "${APP_DIR}/" || { echo 'ERROR: Unable to create app directory'; exit 1; }
+    rm -rf "${ARTIFACT_DIR}/" || { echo 'ERROR: Unable to delete artifact directory'; exit 1; }
+    mkdir -p "${APP_DIR}/" || { echo 'ERROR: Unable to create app directory'; exit 1; }
 
-./build.sh || { echo 'ERROR: Unable to build project'; exit 1; }
+    ./build.sh || { echo 'ERROR: Unable to build project'; exit 1; }
 
-mv dist "${APP_DIR}/${APP_TAG}" || { echo 'ERROR: Unable to move app artifact directory'; exit 1; }
+    mv dist "${APP_DIR}/${APP_TAG}" || { echo 'ERROR: Unable to move app artifact directory'; exit 1; }
 
-tar -c -z -f "${APP_DIR}/${APP_TAG}.tar.gz" -C "${APP_DIR}" "${APP_TAG}" || { echo 'ERROR: Unable to create artifact'; exit 1; }
+    tar -c -z -f "${APP_DIR}/${APP_TAG}.tar.gz" -C "${APP_DIR}" "${APP_TAG}" || { echo 'ERROR: Unable to create artifact'; exit 1; }
+fi
+
+if [ "${TRAVIS_BRANCH:-}" == "master" -o -n "${TRAVIS_TAG:-}" ]; then
+    DOCKER_REPO="tidepool/${TRAVIS_REPO_SLUG#*/}"
+
+    echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+
+    docker build --tag "${DOCKER_REPO}:development" --target=development .
+    docker build --tag "${DOCKER_REPO}" .
+    if [ "${TRAVIS_BRANCH:-}" == "master" ]; then
+        docker push "${DOCKER_REPO}:development"
+        docker push "${DOCKER_REPO}"
+    fi
+    if [ -n "${TRAVIS_TAG:-}" ]; then
+        docker tag "${DOCKER_REPO}" "${DOCKER_REPO}:${TRAVIS_TAG}"
+        docker push "${DOCKER_REPO}:${TRAVIS_TAG}"
+    fi
+fi
--- artifact/artifact_node.sh
+++ artifact/artifact_node.sh
@@ -1,36 +1,47 @@
-#!/bin/sh -e
-
-if [ -z "${TRAVIS_TAG}" ]; then
-    exit 0
-fi
-
-set -u
+#!/bin/bash -eu
 
 if [ "${TRAVIS_NODE_VERSION}" != "${ARTIFACT_NODE_VERSION}" ]; then
     exit 0
 fi
 
-ARTIFACT_DIR='deploy'
+if [ -n "${TRAVIS_TAG:-}" ]; then
+    ARTIFACT_DIR='deploy'
 
-APP="${TRAVIS_REPO_SLUG#*/}"
-APP_DIR="${ARTIFACT_DIR}/${APP}"
-APP_TAG="${APP}-${TRAVIS_TAG}"
+    APP="${TRAVIS_REPO_SLUG#*/}"
+    APP_DIR="${ARTIFACT_DIR}/${APP}"
+    APP_TAG="${APP}-${TRAVIS_TAG}"
 
-TMP_DIR="/tmp/${TRAVIS_REPO_SLUG}"
+    TMP_DIR="/tmp/${TRAVIS_REPO_SLUG}"
 
-if [ -f '.artifactignore' ]; then
-    RSYNC_OPTIONS='--exclude-from=.artifactignore'
-else
-    RSYNC_OPTIONS=''
+    if [ -f '.artifactignore' ]; then
+        RSYNC_OPTIONS='--exclude-from=.artifactignore'
+    else
+        RSYNC_OPTIONS=''
+    fi
+
+    rm -rf "${ARTIFACT_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to delete artifact and tmp directories'; exit 1; }
+    mkdir -p "${APP_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to create app and tmp directories'; exit 1; }
+
+    ./build.sh || { echo 'ERROR: Unable to build project'; exit 1; }
+
+    rsync -a ${RSYNC_OPTIONS} . "${TMP_DIR}/${APP_TAG}/" || { echo 'ERROR: Unable to copy files'; exit 1; }
+
+    tar -c -z -f "${APP_DIR}/${APP_TAG}.tar.gz" -C "${TMP_DIR}" "${APP_TAG}" || { echo 'ERROR: Unable to create artifact'; exit 1; }
+
+    rm -rf "${TMP_DIR}/"
 fi
 
-rm -rf "${ARTIFACT_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to delete artifact and tmp directories'; exit 1; }
-mkdir -p "${APP_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to create app and tmp directories'; exit 1; }
+if [ "${TRAVIS_BRANCH:-}" == "master" -o -n "${TRAVIS_TAG:-}" ]; then
+    DOCKER_REPO="tidepool/${TRAVIS_REPO_SLUG#*/}"
 
-./build.sh || { echo 'ERROR: Unable to build project'; exit 1; }
+    echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
 
-rsync -a ${RSYNC_OPTIONS} . "${TMP_DIR}/${APP_TAG}/" || { echo 'ERROR: Unable to copy files'; exit 1; }
-
-tar -c -z -f "${APP_DIR}/${APP_TAG}.tar.gz" -C "${TMP_DIR}" "${APP_TAG}" || { echo 'ERROR: Unable to create artifact'; exit 1; }
-
-rm -rf "${TMP_DIR}/"
+    docker build --tag "${DOCKER_REPO}" .
+    if [ "${TRAVIS_BRANCH:-}" == "master" ]; then
+        docker push "${DOCKER_REPO}"
+    fi
+    if [ -n "${TRAVIS_TAG:-}" ]; then
+        docker tag "${DOCKER_REPO}" "${DOCKER_REPO}:${TRAVIS_TAG}"
+        docker push "${DOCKER_REPO}:${TRAVIS_TAG}"
+    fi
+fi
