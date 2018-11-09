707d04dcda773547b3182307500d40656e870f60
Franck Fourel <franck@diabeloop.fr>
2018-11-09 11:01:30 +0100

    Enable docker publish (#3)
    * enable docker publish for node builds
    
    * Enable docker publish for go build

--- artifact/artifact_node.sh
+++ artifact/artifact_node.sh
@@ -31,19 +31,17 @@ if [ -n "${TRAVIS_TAG:-}" ]; then
     rm -rf "${TMP_DIR}/"
 fi
 
-# Let's disable the docker section for now
+if [ "${TRAVIS_BRANCH:-}" == "master" -a "${TRAVIS_PULL_REQUEST_BRANCH:-}" == "" -o -n "${TRAVIS_TAG:-}" ]; then
+    DOCKER_REPO="docker.ci.diabeloop.eu/${TRAVIS_REPO_SLUG#*/}"
 
-# if [ "${TRAVIS_BRANCH:-}" == "master" -a "${TRAVIS_PULL_REQUEST_BRANCH:-}" == "" -o -n "${TRAVIS_TAG:-}" ]; then
-#     DOCKER_REPO="tidepool/${TRAVIS_REPO_SLUG#*/}"
-#
-#     echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
-#
-#     docker build --tag "${DOCKER_REPO}" .
-#     if [ "${TRAVIS_BRANCH:-}" == "master" -a "${TRAVIS_PULL_REQUEST_BRANCH:-}" == "" ]; then
-#         docker push "${DOCKER_REPO}"
-#     fi
-#     if [ -n "${TRAVIS_TAG:-}" ]; then
-#         docker tag "${DOCKER_REPO}" "${DOCKER_REPO}:${TRAVIS_TAG}"
-#         docker push "${DOCKER_REPO}:${TRAVIS_TAG}"
-#     fi
-# fi
+    echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+
+    docker build --tag "${DOCKER_REPO}" .
+    if [ "${TRAVIS_BRANCH:-}" == "master" -a "${TRAVIS_PULL_REQUEST_BRANCH:-}" == "" ]; then
+        docker push "${DOCKER_REPO}"
+    fi
+    if [ -n "${TRAVIS_TAG:-}" ]; then
+        docker tag "${DOCKER_REPO}" "${DOCKER_REPO}:${TRAVIS_TAG}"
+        docker push "${DOCKER_REPO}:${TRAVIS_TAG}"
+    fi
+fi
