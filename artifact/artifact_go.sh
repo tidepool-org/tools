6827309b864ad457d4e0f40f509fb4a967948956
Derrick Burns <derrickrburns@gmail.com>
2019-04-23 17:49:25 -0700

    This is the first in a multi-step process to evolve our CI process to work with Kubernetes.
    Each of the non-platform services pulls an artifact generating file from
    GitHub as a raw file. Then, it uses that file to generate artifacts.
    We want to generate Docker images and push them to DockerHub.
    Changing these artifact generating files is the place to do so.
    
    In this first step, we merge the two artifact generating files into
    one, but make three identical copies of that file to address the three
    distinct cases:
      1) create go tarballs and Docker images;
      2) create node tarballs and Docker images; or,
      3) only create Docker images (to be used with `viz`).
    
    Ideally we could use symbolic links.  However, without simultaneously
    changing all the services that use this code, this is not possible.
    Each service repo uses `wget` to retrieve these artifact generating files.
    Wget on GitHub raw files does not resolve symbolic links properly.
    
    After this change is committed to master, we can commence the second step:
    to modify the service repos to retrieve the same `artifact.sh` file.
    Then, the service can pass in an argument to indicate which type of
    artifacts to create.
    
    In the third step, after all modifications have percolated to
    all outstanding branches of all services, we delete the files `artifact_go.sh`
    and `artifact_node.sh` herein. That eliminates the duplication.
    
    In the fourth step, when we have shifted completed to K8s, we modify
    `artifact.sh` to not create tarballs. :)

--- artifact/artifact_go.sh
+++ artifact/artifact_go.sh
@@ -1,43 +1,119 @@
 #!/bin/bash -eux
+#
+# This code is meant to be executed within a TravisCI build. 
 
-if [ "${TRAVIS_GO_VERSION}" != "${ARTIFACT_GO_VERSION}" ]; then
-    echo "Travis GO version does not match Artifact Go Version"
-    exit 1
-fi
-
-if [ -n "${TRAVIS_TAG:-}" ]; then
-    ARTIFACT_DIR='deploy'
-
-    APP="${TRAVIS_REPO_SLUG#*/}"
-    APP_DIR="${ARTIFACT_DIR}/${APP}"
-    APP_TAG="${APP}-${TRAVIS_TAG}"
-
-    rm -rf "${ARTIFACT_DIR}/" || { echo 'ERROR: Unable to delete artifact directory'; exit 1; }
-    mkdir -p "${APP_DIR}/" || { echo 'ERROR: Unable to create app directory'; exit 1; }
-
-    ./build.sh || { echo 'ERROR: Unable to build project'; exit 1; }
-
-    mv dist "${APP_DIR}/${APP_TAG}" || { echo 'ERROR: Unable to move app artifact directory'; exit 1; }
-
-    tar -c -z -f "${APP_DIR}/${APP_TAG}.tar.gz" -C "${APP_DIR}" "${APP_TAG}" || { echo 'ERROR: Unable to create artifact'; exit 1; }
-fi
-
-if [ -n "${DOCKER_USERNAME:-}" -a -n "${DOCKER_PASSWORD:-}"  ]; then
-    DOCKER_REPO="tidepool/${TRAVIS_REPO_SLUG#*/}"
-    echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
-    docker build --tag ${DOCKER_REPO} .
-    
-    if [ "${TRAVIS_BRANCH:-}" == "master" -a "${TRAVIS_PULL_REQUEST_BRANCH:-}" == "" ]; then
-        docker push ${DOCKER_REPO}
+check_go_version() {
+    if [ "${TRAVIS_GO_VERSION}" != "${ARTIFACT_GO_VERSION}" ]; then
+        echo "Travis GO version does not match Artifact Go Version"
+        exit 1
     fi
+}
+
+build_go_artifact() {
     if [ -n "${TRAVIS_TAG:-}" ]; then
-        docker tag ${DOCKER_REPO} ${DOCKER_REPO}:${TRAVIS_TAG}
-        docker push ${DOCKER_REPO}:${TRAVIS_TAG}
+        ARTIFACT_DIR='deploy'
+    
+        APP="${TRAVIS_REPO_SLUG#*/}"
+        APP_DIR="${ARTIFACT_DIR}/${APP}"
+        APP_TAG="${APP}-${TRAVIS_TAG}"
+    
+        rm -rf "${ARTIFACT_DIR}/" || { echo 'ERROR: Unable to delete artifact directory'; exit 1; }
+        mkdir -p "${APP_DIR}/" || { echo 'ERROR: Unable to create app directory'; exit 1; }
+    
+        ./build.sh || { echo 'ERROR: Unable to build project'; exit 1; }
+    
+        mv dist "${APP_DIR}/${APP_TAG}" || { echo 'ERROR: Unable to move app artifact directory'; exit 1; }
+    
+        tar -c -z -f "${APP_DIR}/${APP_TAG}.tar.gz" -C "${APP_DIR}" "${APP_TAG}" || { echo 'ERROR: Unable to create artifact'; exit 1; }
     fi
-    if [ -n "${TRAVIS_BRANCH:-}" -a -n "${TRAVIS_COMMIT:-}" ]; then
-        docker tag ${DOCKER_REPO} ${DOCKER_REPO}:${TRAVIS_BRANCH}-${TRAVIS_COMMIT}
-        docker push ${DOCKER_REPO}:${TRAVIS_BRANCH}-${TRAVIS_COMMIT}
+}
+
+    if [ "${TRAVIS_NODE_VERSION}" != "${ARTIFACT_NODE_VERSION}" ]; then
+        echo "Travis Node version does not match Artifact Node version"
+        exit 1
     fi
-else
-    echo "Missing DOCKER_USERNAME or DOCKER_PASSWORD."
-fi
+}
+
+build_node_artifact() {
+    if [ -n "${TRAVIS_TAG:-}" ]; then
+        ARTIFACT_DIR='deploy'
+    
+        APP="${TRAVIS_REPO_SLUG#*/}"
+        APP_DIR="${ARTIFACT_DIR}/${APP}"
+        APP_TAG="${APP}-${TRAVIS_TAG}"
+    
+        TMP_DIR="/tmp/${TRAVIS_REPO_SLUG}"
+    
+        if [ -f '.artifactignore' ]; then
+            RSYNC_OPTIONS='--exclude-from=.artifactignore'
+        else
+            RSYNC_OPTIONS=''
+        fi
+    
+        rm -rf "${ARTIFACT_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to delete artifact and tmp directories'; exit 1; }
+        mkdir -p "${APP_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to create app and tmp directories'; exit 1; }
+    
+        ./build.sh || { echo 'ERROR: Unable to build project'; exit 1; }
+    
+        rsync -a ${RSYNC_OPTIONS} . "${TMP_DIR}/${APP_TAG}/" || { echo 'ERROR: Unable to copy files'; exit 1; }
+    
+        tar -c -z -f "${APP_DIR}/${APP_TAG}.tar.gz" -C "${TMP_DIR}" "${APP_TAG}" || { echo 'ERROR: Unable to create artifact'; exit 1; }
+    
+        rm -rf "${TMP_DIR}/"
+    fi
+}
+
+publish_to_dockerhub() {
+    if [ -n "${DOCKER_USERNAME:-}" -a -n "${DOCKER_PASSWORD:-}"  ]; then
+        DOCKER_REPO="tidepool/${TRAVIS_REPO_SLUG#*/}"
+        echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+        docker build --tag ${DOCKER_REPO} .
+        
+        if [ "${TRAVIS_BRANCH:-}" == "master" -a "${TRAVIS_PULL_REQUEST_BRANCH:-}" == "" ]; then
+            docker push ${DOCKER_REPO}
+        fi
+        if [ -n "${TRAVIS_TAG:-}" ]; then
+            docker tag ${DOCKER_REPO} ${DOCKER_REPO}:${TRAVIS_TAG}
+            docker push ${DOCKER_REPO}:${TRAVIS_TAG}
+        fi
+        if [ -n "${TRAVIS_BRANCH:-}" -a -n "${TRAVIS_COMMIT:-}" ]; then
+            docker tag ${DOCKER_REPO} ${DOCKER_REPO}:${TRAVIS_BRANCH}-${TRAVIS_COMMIT}
+            docker push ${DOCKER_REPO}:${TRAVIS_BRANCH}-${TRAVIS_COMMIT}
+        fi
+    else
+        echo "Missing DOCKER_USERNAME or DOCKER_PASSWORD."
+    fi
+}
+
+# Allow encoding of behavior in file name for backward compatibility.
+me=`basename "$0"`
+case $me in
+	artifact_go.sh)
+		proc=go
+		;;
+	artifact_node.sh)
+		proc=node
+		;;
+	*)
+		proc=$1
+		;;
+esac
+
+case $proc in 
+	go)
+		echo "Handling go artifact"
+		check_go_version
+		build_go_artifact
+		publish_to_dockerhub
+		;;
+	node)
+		echo "Handling node artifact"
+		check_node_version
+		build_node_artifact
+		publish_to_dockerhub
+		;;
+	*)
+		echo "Just publishing to Docker Hub"
+		publish_to_dockerhub
+		;;
+esac
