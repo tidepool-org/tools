0ff3e3308b642ce1ad9e32997792271214e7b026
Chris McGee <chris@tidepool.org>
2023-09-15 21:07:02 -0400

    treat uploader the same as blip

--- artifact/artifact.sh
+++ artifact/artifact.sh
@@ -39,11 +39,10 @@ publish_to_dockerhub() {
         DOCKER_REPO="tidepool/${REPO_SLUG#*/}"
 
         echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
-
-        if [ "${REPO_SLUG:-}" == "tidepool-org/blip" ]; then
+        if [ "${REPO_SLUG:-}" == "tidepool-org/blip" ] || [ "${REPO_SLUG:-}" == "tidepool-org/uploader" ]; then
             # Build blip image
             RX_ENABLED=${RX_ENABLED-false}
-            if [[ ",${RX_ENABLED_BRANCHES:-}," = *",${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH},"* ]]; then RX_ENABLED=true; fi
+            if [[ ",${RX_ENABLED_BRANCHES:-}," = *",${PR_BRANCH:-$BRANCH},"* ]]; then RX_ENABLED=true; fi
             DOCKER_BUILDKIT=1 docker build --tag "${DOCKER_REPO}" --build-arg ROLLBAR_POST_SERVER_TOKEN="${ROLLBAR_POST_SERVER_TOKEN:-}" --build-arg LAUNCHDARKLY_CLIENT_TOKEN="${LAUNCHDARKLY_CLIENT_TOKEN:-}" --build-arg REACT_APP_GAID="${REACT_APP_GAID:-}" --build-arg RX_ENABLED="${RX_ENABLED:-}" --build-arg TRAVIS_COMMIT="${COMMIT:-}" .
         else
             # Build other images
